72e9393078b5b14973e4c394266e795a7e3c40a7
yakov <jakob0417@gmail.com>
2020-11-07 22:21:35 +0200

    items in cart edit/remove

--- app/src/main/java/com/example/skyrunexample/CartActivity.java
+++ app/src/main/java/com/example/skyrunexample/CartActivity.java
@@ -1,22 +1,28 @@
 package com.example.skyrunexample;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.skyrunexample.Model.Cart;
 import com.example.skyrunexample.Prevalent.Prevalent;
 import com.example.skyrunexample.ViewHolder.CartViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -65,11 +71,62 @@ public class CartActivity extends AppCompatActivity
                 = new FirebaseRecyclerAdapter<Cart, CartViewHolder>(options)
         {
             @Override
-            protected void onBindViewHolder(@NonNull CartViewHolder holder, int position, Cart cart)
+            protected void onBindViewHolder(@NonNull CartViewHolder holder, int position, final Cart cart)
             {
                 holder.txtProductQuantity.setText("Quantity" + cart.getQuantity());
                 holder.txtProductPrice.setText("Price " + cart.getPrice() +"$");
                 holder.txtProductName.setText(cart.getPname());
+
+                holder.itemView.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view)
+                    {
+                        CharSequence options[] = new CharSequence[]
+                                {
+                                      "Edit",
+                                      "Remove"
+                                };
+                        AlertDialog.Builder builder = new AlertDialog.Builder(CartActivity.this);
+                        builder.setTitle("Cart Options");
+
+                        builder.setItems(options, new DialogInterface.OnClickListener()
+                        {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i)
+                            {
+                                if (i == 0)
+                                {
+                                    Intent intent = new Intent(CartActivity.this, ProductDetailsActivity.class);
+                                    intent.putExtra("pid", cart.getPid());
+                                    startActivity(intent);
+                                }
+                                if (i == 1)
+                                {
+                                    cartListRef.child("User View")
+                                            .child(Prevalent.currentOnlineUser.getPhone())
+                                            .child("Products")
+                                            .child(cart.getPid())
+                                            .removeValue()
+                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task)
+                                                {
+                                                    if (task.isSuccessful())
+                                                    {
+                                                        Toast.makeText(CartActivity.this, "Item is removed", Toast.LENGTH_SHORT).show();
+
+                                                        Intent intent = new Intent(CartActivity.this, HomeActivity.class);
+                                                        startActivity(intent);
+                                                    }
+                                                }
+                                            });
+                                }
+                            }
+                        });
+                        builder.show();
+                    }
+                                                   }
+                );
             }
 
             @NonNull
